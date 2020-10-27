793bca77d3d5746b027d5b1acebca89c8fd1c5af
RooXChicken <rooxchicken@gmail.com>
2020-10-26 20:45:38 -0400

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+#when using these, you dont have to specify sf:: or std:: in front of certain functions.
+using namespace std;
+using namespace sf;
+
+int main( int argc, char** argv )
+{
+	RenderWindow appWindow( sf::VideoMode( 800, 600, 32 ), "App" );
+	Event appEvent;
+	
+	while ( appWindow.isOpen() )
+	{
+		while ( appWindow.pollEvent( appEvent ) )
+		{
+			if ( appEvent.type == Event::Closed )
+				appWindow.close();
+		}
+		
+		appWindow.clear();
+		appWindow.display();
+	}
+	
+	return 0;
+}
